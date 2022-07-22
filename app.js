915ef1cd75d66115ed412ee09ac1e84e5146d486
Nataly Sagalaeva <garna@yandex.ru>
2022-07-22 18:09:20 +0300

    app.js2

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const router = express.Router();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const path = require('path');
 
 // Слушаем 3000 порт
 const { PORT = 3000 } = process.env;
