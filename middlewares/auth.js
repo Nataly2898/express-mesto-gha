51fcfa9f57934050b42537364ce690eab065e494
Nataly Sagalaeva <garna@yandex.ru>
2022-08-04 15:56:56 +0300

    10 доработки

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -7,11 +7,12 @@ module.exports = (req, res, next) => {
 
   // убеждаемся, что он есть или начинается с Bearer
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    throw new NotAuthorizationError('Необходима авторизация.');
+    return next(new NotAuthorizationError('Необходима авторизация.'));
   }
 
   // извлечём токен
   const token = authorization.replace('Bearer ', '');
+
   let payload;
 
   try {
@@ -19,7 +20,7 @@ module.exports = (req, res, next) => {
     payload = jwt.verify(token, 'some-secret-key');
   } catch (err) {
     // отправим ошибку, если не получилось
-    throw new NotAuthorizationError('Необходима авторизация.');
+    return next(new NotAuthorizationError('Необходима авторизация.'));
   }
 
   req.user = payload;
